6414dcda982548ad503ab71ee1189c08ada4e52d
Ryan <cubis-git.3gyz7@simplelogin.co>
2025-03-08 22:12:05 -0500

    get script working

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import os
 import re
 from random import choices
 
+import in_place
+
 
 def load_file():
     lines = list[str]()
     with open(os.getenv("DWT_TAGLINEFILE", "r")) as f:
-        line_re = re.compile("\s+\*\s+(.+)\s*")
+        line_re = re.compile("\s+\*\s+(.+)")
         for line in f:
             m = line_re.match(line)
             if m:
                 lines.append(m.group(1))
     return lines
+
+
+def update_config(items: list[str]):
+    php_prop = "$conf['plugin']['tplmod']['taglines']"
+    config_file = os.getenv("DWT_CONFIGFILE")
+    with in_place.InPlace(config_file, backup_ext=".bak") as f:
+        for line in f:
+            if php_prop in line:
+                f.write(f"${php_prop} = '${items}';\n")
+            else:
+                f.write(line)
 
 
 def main():
@@ -21,6 +34,7 @@ def main():
             [x.replace(",", ";").replace("'", "").strip() for x in choices(lines, k=7)]
         )
         print(items)
+        update_config(items)
 
 
 if __name__ == "__main__":
